e7fc6d24bfdd03dd92cfdeea751e569208dbc31c
Guilherme Delgado Martins <guilhermedelgado876@gmail.com>
2024-05-29 09:14:18 -0300

    feat: inclementar modelo da entidade Character

---
+++ MODEL/Character.Model.php
@@ -0,0 +1,45 @@
+<?php 
+  namespace MODEL; 
+  class Account {
+       private ?int $id; 
+       private ?string $name;
+       private ?int $class; 
+       private ?int $idAttribute;
+
+       public function __construct() { }
+
+       public function getID(){
+           return $this->id; 
+       }
+
+       public function setId(int $id){
+          $this->id = $id;     
+       }
+
+       public function getName(){
+          return $this->name;       
+       }
+
+       public function setName(string $name){
+          $this->name = $name; 
+       }
+
+       public function getClass(){
+         return $this->class;       
+      }
+
+      public function setClass(string $class){
+        $this->class = $class; 
+      }
+
+      public function getIdAttribute(){
+        return $this->idAttribute;       
+      }
+
+      public function setIdAttribute(string $idAttribute){
+        $this->idAttribute = $idAttribute; 
+      }
+     
+  }
+
+?>
